65b72e629775a736c7fe7c4e95d51eefd006c1c7
Adil Serroukh <a.serroukh.ext@orangebank.es>
2020-07-03 02:08:48 +0200

    Creacion esqueleto del proyecto

---
+++ calculadora-acceptance-test/src/main/java/com/atsistemas/poc/LauncherAcceptanceTest.java
@@ -0,0 +1,14 @@
+package com.atsistemas.poc;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LauncherAcceptanceTest {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LauncherAcceptanceTest.class, args);
+    }
+
+}
---
+++ calculadora-app/src/test/java/com/atsistemas/poc/LauncherAppTest.java
@@ -0,0 +1,13 @@
+package com.atsistemas.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
